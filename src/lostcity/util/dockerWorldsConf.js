dc4abe2257d894681af57ce71ccdbb0322729239
Rohanlogs <rohanlogs@gmail.com>
2025-01-10 16:37:20 +0200

    feat(infra): Attempt to pass worlds from server repository to composer, to website container

--- src/lostcity/util/dockerWorldsConf.js
+++ src/lostcity/util/dockerWorldsConf.js
@@ -1,24 +1,15 @@
 import fs from 'fs';
 import path from 'path';
-
-const worldConfig = [
-    {
-        id: 10,
-        region: process.env.REGION || 'Docker build test',
-        address: `http://localhost:${process.env.WEB_PORT || 3000}`,
-        members: process.env.MEMBERS_WORLD === 'true',
-        portOffset: 0
-    }
-];
+import worldConfig from '/usr/src/app/prodWorldsConfig.js';
 
 const filePath = path.resolve('/usr/src/app/data/config/worlds.json');
 
-fs.mkdirSync(path.dirname(filePath), { recursive: true });
+fs.mkdirSync(path.dirname(filePath), {recursive: true});
 fs.writeFileSync(filePath, JSON.stringify(worldConfig, null, 2));
 
 console.log('[INFO] Worlds config generated! Much wow ');
 console.log(`
     (\\(\\ 
     ( -.-) ☆彡
-    o_(")(")   You're a star!`
+    o_(")(")   You're a star!\n`
 );
